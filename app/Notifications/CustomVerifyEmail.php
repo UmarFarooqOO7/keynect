705dd1346874bca24f970bba5c7ec5bf8ca76940
Umar <arrhammzain@gmail.com>
2025-02-20 17:00:35 +0500

    Implement queued email verification notification for improved performance

--- app/Notifications/CustomVerifyEmail.php
+++ app/Notifications/CustomVerifyEmail.php
@@ -2,14 +2,18 @@
 
 namespace App\Notifications;
 
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\URL;
 
-class CustomVerifyEmail extends Notification
+class CustomVerifyEmail extends Notification implements ShouldQueue
 {
+    use Queueable;
+
     protected string $password;
 
     public function __construct(string $password)
