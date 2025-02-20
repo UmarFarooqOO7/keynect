922b2b1d1ac1de25e32fcfc28583a3f3a35e0262
Umar <arrhammzain@gmail.com>
2025-02-20 15:27:39 +0500

    Update primary and secondary colors in Filament admin panel configuration

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -32,7 +32,8 @@ class AdminPanelProvider extends PanelProvider
             ->emailVerification()
             ->profile(isSimple: false)
             ->colors([
-                'primary' => Color::Blue,
+                'primary' => '#1ABA9C',
+                'secondary' => '#294b33',
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
